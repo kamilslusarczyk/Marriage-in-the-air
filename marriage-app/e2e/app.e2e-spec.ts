efbea60a3917a72d42c037de3f8454d9ab8ecc53
Michał Garbacz <michal.garbacz@atos.net>
2017-11-19 14:05:05 +0100

    @angular/cli new version v2

--- marriage-app/e2e/app.e2e-spec.ts
+++ marriage-app/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { MarriageAppPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('marriage-app App', function() {
-  let page: MarriageAppPage;
+describe('marriage-app App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new MarriageAppPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
