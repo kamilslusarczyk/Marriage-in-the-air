fa32848f4a374878f59b0799dadad58fce187cb2
Michał Garbacz <michal.garbacz@atos.net>
2017-11-25 10:41:34 +0100

    fix #1. after Kamil's commit

--- marriage-app/assets/app/app.component.ts
+++ marriage-app/assets/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { Message } from './messages/message.model';
-import { MessageService } from './messages/message.service';
+
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    providers: [MessageService]
+    providers: []
 })
 export class AppComponent {
 
